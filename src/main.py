985e92fcd3eeb50f4e28228a12915c428383556f
issacleral <rodrigosalvatierracalla@gmail.com>
2025-04-17 22:38:55 +0200

    conexion de html  a python

--- src/main.py
+++ src/main.py
@@ -11,17 +11,35 @@ application.config['CORS_HEADERS'] = 'Content-Type'
 
 
 #CONFIGURACION DE LA PAGINA
-@application.route("/api", methods=["GET"])
+@application.route("/", methods=["GET", "POST"])
 def inicio():
     return abrir_html()
 
 #ABRIR EL HTML 
 def abrir_html():
     try:
-        with open("src/index.html", encoding="utf-8") as f:
+        with open("index.html", encoding="utf-8") as f:
             return f.read()
     except FileNotFoundError:
         return "<bold>Archivo no encontrado</bold>"
+    
+
+# CONEXION A LA BASE DE DATOS
+@application.route("/conexion", methods=["GET"])
+def conexion():
+    conexion = SQLiteConnection("Database1.db")
+    classes = conexion.execute_query("SELECT * FROM clases ")
+    return classes
+
+#  BUSCADOR DE CATEGORIAS 
+@application.route("/search", methods=["GET"])
+def buscador():
+    datos_usuario = request.args.get("query")
+
+    if datos_usuario is None:
+        return "No hay datos que buscar"
+       
+    return "Resultados de la busqueda " + str(datos_usuario)
 
 # RUTA PARA AGREGAR LA CATEGORIA
 @application.route("/api/agregar_categoria", methods=["POST"])
